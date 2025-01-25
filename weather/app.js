bda8844de90a97a5d9edc4eceb964564841751eb
sandeshgheware07 <sandeshgheware777@gmail.com>
2025-01-25 15:41:46 +0530

    devenv

--- weather/app.js
+++ weather/app.js
@@ -31,7 +31,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // rendering the error page
   res.status(err.status || 500);
   res.render('error');
 });
