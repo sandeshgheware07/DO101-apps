4a86b9c653491b508e4219401cb3245027dffc71
sandeshgheware07 <sandeshgheware777@gmail.com>
2025-01-25 15:28:52 +0530

    updated contacts

--- contacts/app.js
+++ contacts/app.js
@@ -24,7 +24,7 @@ app.use('/', indexRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+//
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
