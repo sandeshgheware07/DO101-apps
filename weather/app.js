9a777e12c22ecf0dc74222f49cc3cf9fdbfb544e
sandeshgheware07 <sandeshgheware777@gmail.com>
2025-01-25 15:37:33 +0530

    troubleshoot

--- weather/app.js
+++ weather/app.js
@@ -31,7 +31,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // render the error in the page
   res.status(err.status || 500);
   res.render('error');
 });
